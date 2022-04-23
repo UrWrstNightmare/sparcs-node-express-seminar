e7f25ed23a13ad0ce3c8ac9cfdae67338fb73855
Jiho Park <jihopark7777@gmail.com>
2022-04-24 07:15:14 +0900

    Stage #2 : Introduction to the app object, routes and connect FE to BE

--- seminar/index.js
+++ seminar/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log('[REQUEST-CORS] Request from origin: ', origin);
+        if (!origin || whitelist.indexOf(origin) !== -1) callback(null, true)
+        else callback(new Error('Not Allowed by CORS'));
+    },
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.status(200).send('Hello, World!');
 });
+
+app.get('/status', (req, res) => {
+    res.status(200).json({ isOnline: true });
+})
 
 app.listen(port, () => {
    console.log(`Example App Listening @ http://localhost:${ port }`);
