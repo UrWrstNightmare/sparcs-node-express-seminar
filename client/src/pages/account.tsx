1ef8591203d13672cec07cb0749ab9bfaf351293
최우석 <oosoi@sparcs.org>
2024-04-09 00:20:04 +0900

    hw2 완료

--- client/src/pages/account.tsx
+++ client/src/pages/account.tsx
@@ -5,14 +5,17 @@ import {SAPIBase} from "../tools/api";
 import "./css/account.css";
 
 const AccountPage = () => {
-  const [ SAPIKEY, setSAPIKEY ] = React.useState<string>("");
+  //const [ SAPIKEY, setSAPIKEY ] = React.useState<string>("");
+  const [ SAPIID, setSAPIID ] = React.useState<string>("");
+  const [ SAPIPW, setSAPIPW ] = React.useState<string>("");
   const [ NBalance, setNBalance ] = React.useState<number | "Not Authorized">("Not Authorized");
   const [ NTransaction, setNTransaction ] = React.useState<number | ''>(0);
 
   const getAccountInformation = () => {
     const asyncFun = async() => {
       interface IAPIResponse { balance: number };
-      const { data } = await axios.post<IAPIResponse>(SAPIBase + '/account/getInfo', { credential: SAPIKEY });
+      //const { data } = await axios.post<IAPIResponse>(SAPIBase + '/account/getInfo', { credential: SAPIKEY });
+      const { data } = await axios.post<IAPIResponse>(SAPIBase + '/account/getInfo', { ID: SAPIID, PW: SAPIPW });
       setNBalance(data.balance);
     }
     asyncFun().catch((e) => window.alert(`AN ERROR OCCURED: ${e}`));
@@ -22,7 +25,8 @@ const AccountPage = () => {
     const asyncFun = async() => {
       if (amount === '') return;
       interface IAPIResponse { success: boolean, balance: number, msg: string };
-      const { data } = await axios.post<IAPIResponse>(SAPIBase + '/account/transaction', { credential: SAPIKEY, amount: amount });
+      //const { data } = await axios.post<IAPIResponse>(SAPIBase + '/account/transaction', { credential: SAPIKEY, amount: amount });
+      const { data } = await axios.post<IAPIResponse>(SAPIBase + '/account/transaction', { ID: SAPIID, PW: SAPIPW, amount: amount });
       setNTransaction(0);
       if (!data.success) {
         window.alert('Transaction Failed:' + data.msg);
@@ -39,16 +43,21 @@ const AccountPage = () => {
     <div className={"account"}>
       <Header/>
       <h2>Account</h2>
-      <div className={"account-token-input"}>
-        Enter API Key: <input type={"text"} value={SAPIKEY} onChange={e => setSAPIKEY(e.target.value)}/>
-        <button onClick={e => getAccountInformation()}>GET</button>
-      </div>
+
+      
       <div className={"account-bank"}>
         <h3>The National Bank of SPARCS API</h3>
         <div className={"balance"}>
           <p className={"balance-title"}>Current Balance</p>
           <p className={"balance-value " + (typeof(NBalance) === "number" ? (NBalance >= 0 ? "balance-positive" : "balance-negative") : "")}>₩ { NBalance }</p>
         </div>
+      <div className={"account-username-input"}>
+        Enter your ID: <input type={"text"} value={SAPIID} onChange={e => setSAPIID(e.target.value)}/>  
+      </div>
+        <div className={"account-password-input"}>
+        Enter your Password: <input type={"password"} value={SAPIPW} onChange={e => setSAPIPW(e.target.value)}/>
+      </div>
+      <button onClick={e => getAccountInformation()}>GET</button>
         <div className={"transaction"}>
           ₩ <input type={"number"} value={NTransaction} min={0} onChange={e => setNTransaction(e.target.value === '' ? '' : parseInt(e.target.value))}/>
           <button onClick={e => performTransaction(NTransaction)}>DEPOSIT</button>
--- seminar/src/middleware/auth.js
+++ seminar/src/middleware/auth.js
@@ -1,12 +1,10 @@
 const authMiddleware = (req, res, next) => {
-    if (req.body.credential === process.env.API_KEY) {
+    const {ID, PW }=req.body;
+    if (ID === process.env.API_ID && PW === process.env.API_PW){
         console.log("[AUTH-MIDDLEWARE] Authorized User");
         next();
     }
-    else {
-        console.log("[AUTH-MIDDLEWARE] Not Authorized User");
-        res.status(401).json({ error: "Not Authorized" });
-    }
+        
 }
 
 module.exports = authMiddleware;
